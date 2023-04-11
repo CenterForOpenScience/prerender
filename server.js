9c68e312b67b9c322e60218ba47a17b7649901fa
Matt Clark <mattclark@cos.io>
2023-04-11 15:55:01 -0400

    Enable throttling by default

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ var server = prerender({
     chromeLocation: '/usr/bin/chromium-browser',
     chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ]
 });
+
+server.use(require('./lib/plugins/throttleToken'));
 server.use(prerender.sendPrerenderHeader());
-server.use(prerender.browserForceRestart());
+// server.use(prerender.browserForceRestart());
 // server.use(prerender.blockResources());
 server.use(prerender.addMetaTags());
 server.use(prerender.removeScriptTags());
