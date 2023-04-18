67588b68d353dcd4e706c4c0000b431b269232a9
mattclark <mattclark@users.noreply.github.com>
2023-04-18 10:17:30 -0400

    Adding prerender-aws-s3-cache module
    * Adding server.js to configmap 
    
    * Adding prerender-aws-s3-cache package dep

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var prerender = require('./lib');
 
 var server = prerender({
     chromeLocation: '/usr/bin/chromium-browser',
-    chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ]
+    chromeFlags: [ '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ]
 });
 
-server.use(require('./lib/plugins/throttleToken'));
-server.use(prerender.sendPrerenderHeader());
+// server.use(require('./lib/plugins/throttleToken'));
+// server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.browserForceRestart());
 // server.use(prerender.blockResources());
-server.use(prerender.addMetaTags());
+// server.use(prerender.addMetaTags());
+// server.use(require('prerender-aws-s3-cache'))
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
