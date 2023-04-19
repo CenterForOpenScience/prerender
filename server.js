a9e73e428511f069a6dd5a711c4a8c5e87055a06
Matt Clark <mattclark@cos.io>
2023-04-19 11:36:06 -0400

    bringing throttle token back

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ var prerender = require('./lib');
 
 var server = prerender({
     chromeLocation: '/usr/bin/chromium-browser',
-    chromeFlags: [ '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ]
+    chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ]
 });
 
-// server.use(require('./lib/plugins/throttleToken'));
+server.use(prerender.throttleToken());
 // server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.browserForceRestart());
 // server.use(prerender.blockResources());
+server.use(prerender.healthCheck());
+server.use(require('prerender-aws-s3-cache'));
+// server.use(require('prerender-redis-cache'));
 // server.use(prerender.addMetaTags());
-// server.use(require('prerender-aws-s3-cache'))
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
